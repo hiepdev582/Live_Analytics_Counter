f0e32e2f746f3fcb0de28749a592793cb10d0ae7
hiepdev582 <hiepdev582@gmail.com>
2026-08-24 17:14:43 +0700

    feature(backend): initialize project

---
+++ back-end/src/test/java/com/hiepnn/live_analytics_counter/LiveAnalyticsCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiepnn.live_analytics_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveAnalyticsCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
